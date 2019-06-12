1692c703a194d1a2b125e7c1005145f853707349
Bao-Linh Le Mai <mlblinh@apcs.vn>
2019-06-12 07:04:41 +0700

    A03

---
+++ A03/Assets/ZeroRun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeroRun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector2 zeroPosition;
+
+    public float speed = 0.2f;
+    void Start()
+    {
+        zeroPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            zeroPosition.x -= speed;
+            GetComponent<Animator>().SetBool("isRunning", true);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            zeroPosition.x += speed;
+            GetComponent<Animator>().SetBool("isRunning", true);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+            GetComponent<Animator>().SetBool("isRunning", false);
+        transform.position = zeroPosition;
+    }
+}
